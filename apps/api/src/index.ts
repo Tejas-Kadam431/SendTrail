19584e51109114bed129ff3955c02ba4f043085b
Tejas Kadam <kadamtejas548@gmail.com>
2026-09-07 11:36:26 +0530

    feat: set up TypeScript API

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app=express();
+
+app.get('/health', (req, res)=>{
+  res.status(200).json({
+    "status": "ok"
+  });
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${PORT}`);
+});
